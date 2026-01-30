f368fff2efff8fe61c1408e2b78d0a9d25e00050
karthikjk-debug <karthikbeesabathini820@gmail.com>
2026-01-30 09:30:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,83 +1,45 @@
 import streamlit as st
+import pandas as pd
 
-# Forces the sidebar to stay open on load
-st.set_page_config(
-    page_title="Java Template Hub", 
-    page_icon="☕", 
-    initial_sidebar_state="expanded"
-)
+st.set_page_config(page_title="Java Helper", initial_sidebar_state="expanded")
 
-# Your expanded list of Java topics
-templates = {
-    "Scanner: 1 Variable": """import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-    }
-}""",
-    "Scanner: 2 Variables": """import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-    }
-}""",
-    "If Statement": """if (condition) {
-    // code here
-}""",
-    "If-Else": """if (condition) {
-    // true
-} else {
-    // false
-}""",
-    "Else-If Ladder": """if (condition1) {
-    // block 1
-} else if (condition2) {
-    // block 2
-} else {
-    // block 3
-}""",
-    "Switch Case": """switch(expression) {
-    case x:
-        // code
-        break;
-    default:
-        // code
-}""",
-    "For Loop": """for (int i = 0; i < 10; i++) {
-    System.out.println(i);
-}""",
-    "While Loop": """int i = 0;
-while (i < 10) {
-    System.out.println(i);
-    i++;
-}""",
-    "Do-While Loop": """int i = 0;
-do {
-    System.out.println(i);
-    i++;
-} while (i < 10);""",
-    "Break Statement": """break;""",
-    "Continue Statement": """continue;"""
-}
+# --- Database Connection ---
+DB_URL = "https://raw.githubusercontent.com/karthikjk-debug/java-template-hub/main/java_database.csv"
 
-# --- Permanent Sidebar ---
+@st.cache_data
+def load_java_data(url):
+    try:
+        # Use sep=None to let pandas figure out the comma formatting automatically
+        data = pd.read_csv(url, quotechar='"', on_bad_lines='skip')
+        return data.dropna()
+    except Exception as e:
+        return pd.DataFrame({"template_name": ["Error"], "code": [f"Connection Error: {e}"]})
+
+df = load_java_data(DB_URL)
+
+# --- Sidebar ---
 st.sidebar.title("📚 Java Menu")
-st.sidebar.write("Use ↑/↓ arrows to navigate")
+selected_name = st.sidebar.radio("Choose a Template:", options=df['template_name'].tolist())
 
-# Using 'radio' allows you to click an option and then use your arrow keys
-selected_template = st.sidebar.radio(
-    "Go to:",
-    list(templates.keys()),
-    index=0
-)
+# --- Main Screen ---
+st.title(f"🚀 {selected_name}")
 
-# --- Main Page ---
-st.title(f"🚀 {selected_template}")
-st.code(templates[selected_template], language='java')
+if not df.empty and selected_name != "Error":
+    selected_code = df[df['template_name'] == selected_name]['code'].values[0]
+    
+    # 1. Display the code
+    st.code(selected_code, language='java')
+    
+    # 2. Add the Download Button
+    # We create a filename by removing spaces from the template name
+    file_name = f"{selected_name.replace(' ', '_')}.java"
+    
+    st.download_button(
+        label="📥 Download .java File",
+        data=selected_code,
+        file_name=file_name,
+        mime="text/x-java"
+    )
 
-# A simple, standard button for a quick celebration
 if st.sidebar.button("Celebrate 🎈"):
     st.balloons()
